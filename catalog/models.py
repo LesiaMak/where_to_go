9254baef078899aa97bba9edfbeba3538f0dff32
LesiaMak <101565600+LesiaMak@users.noreply.github.com>
2023-12-12 16:37:48 +0400

    Add Place model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField('Название', max_length=200)
+    description_short = models.TextField('Короткое описание', blank=True)
+    description_long = models.TextField('Расширенное описание', blank=True)
+    lng = models.FloatField("Долгота", blank=True)
+    lat = models.FloatField("Широта", blank=True)
+
+    def __str__(self):
+        return self.title
